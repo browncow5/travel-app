4283eb50fefeaa201e4931d44d4045d8244909c9
browncow5 <ryan.e.donat@gmail.com>
2020-12-30 15:19:55 +0000

    dynamically set list items from dynamo

--- src/App.js
+++ src/App.js
@@ -36,6 +36,79 @@ class AddPlace extends Component{
   }
 }
 
+class Item extends Component {
+  constructor (props){
+    super ();
+    this.state = {
+      name: "not set",
+      id : "-1"
+    };
+  }
+  render (){
+    let text = this.props.name;
+    return (
+        <div className="row">
+          <div className="col-md-12">
+            <input type="checkbox"  />&nbsp;{text}
+            <hr />
+          </div>
+        </div>
+    );
+  }
+}
+
+class ItemList extends Component {
+  constructor (props){
+    super ();
+    this.state = {
+      places: Array(3).fill("initArrayVal")
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const allPlaces = await API.graphql(graphqlOperation(listPlaces));
+      let placeArray = allPlaces.data.listPlaces.items;
+
+      const places = this.state.places.slice();      
+      console.log(places);
+      
+      //console.log(placeArray.forEach(element => console.log(element.name)));
+      
+      for (let index = 0; index < placeArray.length; index++) {
+        const element = placeArray[index];
+        places[index] = element.name;
+        console.log(places[index]);
+      }
+
+      this.setState({places: places});
+
+      console.log(allPlaces);
+    } catch (error) {
+      console.log("Error retrieving Places", error);
+    }
+  }
+
+  renderPlace(i, name){
+    return <Item message={name} value={this.state.Places}/>;
+  }
+
+  render (){
+    let item1 = <Item key="0" name={this.state.places[0]} />;
+    let item2 = <Item key="1" name={this.state.places[1]} />;
+    let item3 = <Item key="2" name={this.state.places[2]} />;
+    let item4 = <Item key="3" name={this.state.places[3]} />;
+
+    let itemArray = [item1, item2, item3, item4];
+    return (
+        <h4>{itemArray}</h4>
+    );
+  }
+  
+}
+
+
+
 class PlaceItem extends Component {
   constructor (props){
     super ();
@@ -69,12 +142,24 @@ class PlaceList extends Component {
     super ();
   }
 
+  async componentDidMount() {
+    try {
+ 
+      const allPlaces = await API.graphql(graphqlOperation(listPlaces));
+      console.log(allPlaces);
+    } catch (error) {
+      console.log("Error retrieving Places", error);
+    }
+  }
+
+
   render (){
+    let item1 = <PlaceItem key="0" message="before update" />;
     let item2 = <PlaceItem key="1" message={sampleJSON.name} />;
     let item3 = <PlaceItem key="2" message="test test test" />;
     let item4 = <PlaceItem key="3" message="one more task" />;
 
-    let allTheThings = [item2, item3, item4];
+    let allTheThings = [item1, item2, item3, item4];
     let items = allTheThings.map(thing => thing);
     return (
         <h4>{items}</h4>
@@ -83,15 +168,16 @@ class PlaceList extends Component {
 }
 
 class ApiObject extends Component {
+  constructor (props){
+    super ();
+  }
   async componentDidMount() {
     try {
-      //const models = await DataStore.query(Place);
-      //console.log(models);
+ 
       const allPlaces = await API.graphql(graphqlOperation(listPlaces));
-      console.log(allPlaces); // result: { "data": { "listTodos": { "items": [/* ..... */] } } }
+      console.log(allPlaces);
     } catch (error) {
       console.log("Error retrieving Places", error);
-      console.log(error.response);
     }
   }
   render (){
@@ -114,8 +200,7 @@ function App() {
         Travel App Admin
       </header>
       <AddPlace></AddPlace>
-      <PlaceList></PlaceList>
-      <ApiObject></ApiObject>
+      <ItemList></ItemList>
     </div>
   );
 }
